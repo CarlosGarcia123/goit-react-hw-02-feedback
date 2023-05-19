0bac810e4058b12aa808e0a98b7790ce8e88e526
CarlosGarcia123 <cegs9403@gmail.com>
2023-05-19 16:52:29 -0500

    TALLER-2-FEEDBACK

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.section`
+  height: 50vh;
+  padding: 50px 200px;
+`;
+
+export const Title = styled.h1`
+  font-size: 40px;
+`;
